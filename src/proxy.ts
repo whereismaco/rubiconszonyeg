62af3a5cdc3d4f44e8e345e24645f37f73c709ed
Istvan Varga <maco@newpush.com>
2026-04-27 13:29:47 +0700

    v1.0

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   if (request.nextUrl.pathname.startsWith('/portal')) {
     if (request.nextUrl.pathname === '/portal/login') {
       return NextResponse.next();
